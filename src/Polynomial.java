479d256009bc7e5546d39833240d99f09207945b
LluisMutA <lluismut@gmail.com>
2024-02-01 20:06:42 +0100

    Multis

--- src/Polynomial.java
+++ src/Polynomial.java
@@ -3,33 +3,13 @@ import java.util.Arrays;
 
 public class Polynomial {
     public Polynomial(float[] cfs) {
-        // TODO: Abans d'emmagatzemar els coeficients, eliminar els zeros de l'esquerra:
-        // EX; [0,0,5,6,0] --> [5,6,0]
-
-        this.cfs = eliminarCeros(cfs);
+        this.cfs = eliminarZeros(cfs);
 
     }
     float[] cfs;
 
     // s s.replaceAll(regex "\\s", replacement "");
     // String[] ar = s.split(regex: "(?=[-+])"); --> Constructe 2
-
-       /* for (float i = 0; i < cfs.length/2; i++) {
-            float j = cfs.length-i-1;
-            float a = cfs[(int) i];
-            float b = cfs[(int) j];
-            cfs[(int) i] = b;
-            cfs[(int) j] = a;
-        }
-
-            if(cfs[i] < 1){
-                cfs[i] = Float.parseFloat(cfs[i]+"x");
-            } else if (cfs[i] == 0) {
-                continue;
-            } else if (cfs[i] > 1) {
-                cfs[i] = Float.parseFloat(coef[i] + "^" + grau);
-            }
-            System.out.println(Arrays.toString(cfs));*/
 
     // variable per guardar el polinomi
     // guardar el valor dels nombres: 3x^3-4x^2-X-3 -->> 3 - 4 - 2 - 1 - 3
@@ -52,12 +32,33 @@ public class Polynomial {
     }
 
     public Polynomial add(Polynomial p) {
-       return null;
+
+        float[] p1 = invertir(this.cfs);
+        float[] p2 = invertir(p.cfs);
+
+        if (p1.length > p2.length){
+            for (int i = 0; i < p2.length; i++) {
+                p1[i] += p2 [i];
+            }invertir(p1);
+            return new Polynomial(p1);
+        }else {
+            for (int i = 0; i < p1.length; i++) {
+                p2[i] += p1[i];
+            }invertir(p2);
+            return new Polynomial(p2);
+        }
     }
 
 
     public Polynomial mult(Polynomial p2) {
-        return null;
+        float[] resultCoefs = new float[this.cfs.length + p2.cfs.length -1];
+
+        for (int i = 0; i < this.cfs.length; i++) {
+            for (int j = 0; j < p2.cfs.length; j++) {
+                resultCoefs[i + j] += this.cfs[i] * p2.cfs[j];
+            }
+        }
+        return new Polynomial(eliminarZeros(resultCoefs));
     }
 
     public Polynomial[] div(Polynomial p2) {
@@ -116,10 +117,9 @@ public class Polynomial {
                 resultat += (int) terme + "x";
             }else resultat += (int) terme + "x^" + grau;
 
-
         }return resultat;
     }
-    private static float[] eliminarCeros(float[] coefs) {
+    private static float[] eliminarZeros(float[] coefs) {
         int primerNoCero = 0;
         // Encontrar el índice del primer elemento no cero
         while (primerNoCero < coefs.length && coefs[primerNoCero] == 0) {
@@ -135,12 +135,24 @@ public class Polynomial {
         if (todosCeros) {
             return new float[]{0};
         } else {
-            // Crear un nuevo array excluyendo los ceros iniciales
+            // Crea un array eliminant els zeros inicials
             float[] res = Arrays.copyOfRange(coefs, primerNoCero, coefs.length);
             return res;
         }
     }
+
+    public static float[] invertir(float[] cfs) { // inverteix l'array
+        for (int i = 0; i < cfs.length/2; i++) {
+            int j = cfs.length-i-1;
+            int a = (int) cfs[i];
+            int b = (int) cfs[j];
+            cfs[i] = b;
+            cfs[j] = a;
+        }
+        return cfs;
+    }
 }
+
 
 //Suma: ar1 = this.coef    Invertim ar1 i ar 2, sumar el petit al gros i girar el resultat i generar polinomi
 //      ar2 = p.coef
